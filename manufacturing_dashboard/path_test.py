9e20f5b3777807840f2d9e51c78148e31d8218a3
RAKESH <rakeshnulekar007@gmail.com>
2025-10-17 11:40:54 +0530

    1. Path tester

---
+++ manufacturing_dashboard/path_test.py
@@ -0,0 +1,13 @@
+import os
+
+#Ruhlamat Machine
+path = r"\\Lip-engg01\8959787117\ufm_8959787117.mdb"
+print("Exists :" ,os.path.exists(path))
+
+#Gauging Machine
+path = r"\\DESKTOP-QVVFPVI\CSV_Data"
+print("Exists :" ,os.path.exists(path))
+
+#EOL Machine
+path = r"\\Cp-57ff06\dc\Q50-11502-0056810"
+print("Exists :" ,os.path.exists(path))
