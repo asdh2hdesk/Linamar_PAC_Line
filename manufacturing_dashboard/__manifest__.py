f84d480627715e2feed9bef00ad2d1e1315a1bbd
Rakesh Nulekar <141513551+rakeshnulekar@users.noreply.github.com>
2025-11-12 12:17:17 +0530

    1. Added users and security for different logins 2. Bottom chatter positioning 3. Menu hide

--- manufacturing_dashboard/__manifest__.py
+++ manufacturing_dashboard/__manifest__.py
@@ -30,6 +30,7 @@ Features:
     },
     'data': [
         'security/ir.model.access.csv',
+        'security/security.xml',
         'data/sequence_data.xml',
         'data/final_station_data.xml',
         'views/part_quality_views.xml',
